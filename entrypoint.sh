01049aaafbef70a686700e6125b5d38bd8d89a44
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-09-25 15:06:17 +0100

    fix migration issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 run_cmd="dotnet run"
+until dotnet ef migrations add initbot; do
+>&2 echo "PostgreSQL is migrating database"
+sleep 1
+done
 until dotnet ef database update; do
->&2 echo "PostgreSQL is starting up"
+>&2 echo "PostgreSQL is updating database"
 sleep 1
 done
 >&2 echo "PostgreSQL is up - executing command"
