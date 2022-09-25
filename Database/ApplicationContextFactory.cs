94c713b8058bf48d16b9d9efe099996716db8dcf
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-09-25 03:24:23 +0100

    something

--- Database/ApplicationContextFactory.cs
+++ Database/ApplicationContextFactory.cs
@@ -1,8 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 namespace Finder.Bot.Database {
-    public class BloggingContextFactory : IDesignTimeDbContextFactory<ApplicationContext> {
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext> {
         public ApplicationContext CreateDbContext(string[] args) {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
             optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")!);
