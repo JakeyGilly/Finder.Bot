21657fbe6a9b7b64c8c0f2a9497e9a8ab60627ca
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-09-25 03:08:31 +0100

    fixed appsettings error

--- Database/ApplicationContextFactory.cs
+++ Database/ApplicationContextFactory.cs
@@ -5,8 +5,7 @@ namespace Finder.Bot.Database {
     public class BloggingContextFactory : IDesignTimeDbContextFactory<ApplicationContext> {
         public ApplicationContext CreateDbContext(string[] args) {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-            IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", false, true).Build();
-            optionsBuilder.UseNpgsql(configuration.GetConnectionString("Default")!, builder => builder.MigrationsAssembly("Finder.Database"));
+            optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")!);
             return new ApplicationContext(optionsBuilder.Options);
         }
     }
